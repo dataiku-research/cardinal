aabfdaa4d662f474a4ae6563548e0b51a604abbc
Alexandre Abraham <alexandre.abraham@dataiku.com>
2019-12-04 13:15:09 +0100

    Remove bad interface

--- cardinal/base.py
+++ cardinal/base.py
@@ -17,17 +17,6 @@ class BaseQuerySampler(ClusterMixin, TransformerMixin, BaseEstimator):
     def predict(self, X):
         raise NotImplementedError
 
-    def transform(self, X):
-        self.labels_ = self.predict(X).astype(bool)
-        return X[self.labels_]
-
-    def inverse_transform(self, X):
-        if self.labels_ is None:
-            return X
-        unmask = self.labels_.copy()
-        unmask[unmask == 1] = X
-        return unmask
-
 
 class ChainQuerySampler(BaseQuerySampler):
     """Allows to whain query sampling methods
