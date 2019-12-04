5a1c3718f05e02b96118a5c1c57cfecd074560f4
Alexandre Abraham <alexandre.abraham@dataiku.com>
2019-12-04 14:55:19 +0100

    Add random sampler

--- cardinAL/random.py
+++ cardinAL/random.py
@@ -1,4 +1,6 @@
 import numpy as np
+from .base import BaseQuerySampler
+from sklearn.utils import check_random_state
 
 
 def random_sampling(X: np.ndarray,
@@ -27,3 +29,62 @@ def random_sampling(X: np.ndarray,
     index = index[:n_instances]
     
     return index, unlabeled[index]
+
+
+class RandomSampler(BaseQuerySampler):
+    """TODO
+    Documentation.
+
+    Parameters
+    ----------
+    batch_size : float, default: 0.05
+        If batch_size < 1., it is interpreted as fraction of the training
+        set to select for labeling. If batch_size >= 1., it is interpreted
+        as the number of samples to draw.
+    verbose : integer, optional
+        The verbosity level
+    random_state : int, RandomState instance or None, optional (default=None)
+        The seed of the pseudo random number generator to use when shuffling
+        the data.  If int, random_state is the seed used by the random number
+        generator; If RandomState instance, random_state is the random number
+        generator; If None, the random number generator is the RandomState
+        instance used by `np.random`.
+
+    Attributes
+    ----------
+    pipeline_ : sklearn.pipeline
+        Pipeline used to predict the class probability.
+    """
+
+    def __init__(self, batch_size=0.05, shuffle=True,
+                 verbose=0, random_state=None,
+                 n_iter_no_change=5, class_weight=None):
+        super().__init__()
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.verbose = verbose
+        self.random_state = random_state
+        self.n_iter_no_change = n_iter_no_change
+        self.class_weight = class_weight
+
+    def fit(self, X, y):
+        """Fit linear model with Stochastic Gradient Descent.
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+            Training data
+        y : numpy array, shape (n_samples,)
+            Target values
+        Returns
+        -------
+        self : returns an instance of self.
+        """
+        self._classes = ['not selected', 'selected']
+        self.random_state = check_random_state(self.random_state)
+        return self
+
+    def predict(self, X):
+        selected_samples = np.zeros(X.shape[0])
+        selected_samples[:self.batch_size] = 1
+        self.random_state.shuffle(selected_samples)
+        return selected_samples
