9e6661a13718a2e2145cbe4272b5f015309cf794
Alexandre Abraham <alexandre.abraham@dataiku.com>
2020-02-27 17:10:34 +0100

    Remove tags in setup

--- setup.py
+++ setup.py
@@ -65,10 +65,7 @@ if __name__ == "__main__":
           classifiers=[
               'Intended Audience :: Science/Research',
               'Intended Audience :: Developers',
-              'License :: OSI Approved',
-              'Programming Language :: C',
               'Programming Language :: Python',
-              'Topic :: Software Development',
               'Topic :: Scientific/Engineering',
               'Operating System :: Microsoft :: Windows',
               'Operating System :: POSIX',
