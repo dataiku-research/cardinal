2a491e1875830e0386a242519c4a52ce2f27a2fa
Alexandre Abraham <alexandre.abraham@dataiku.com>
2020-11-04 10:25:02 +0100

    Iter

--- doc/conf.py
+++ doc/conf.py
@@ -46,6 +46,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx_gallery.gen_gallery',
+    'sphinx_autodoc_annotation'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
