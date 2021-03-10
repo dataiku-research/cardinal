9d73fafc5c9026ea654cd8162cd4b6865b03b2f2
dsleo <leooleds@gmail.com>
2021-03-10 11:16:33 +0100

    fix

--- cardinal/kmeans.py
+++ cardinal/kmeans.py
@@ -119,8 +119,8 @@ def _k_init(X, n_clusters, x_squared_norms, random_state, n_local_trials=None,
     closest_dist_sq = euclidean_distances(
         centers[:start_centers], X, Y_norm_squared=x_squared_norms,
         squared=True)
-    candidates_pot = closest_dist_sq.min(axis=0)
-    current_pot = candidates_pot.sum()
+    closest_dist_sq = closest_dist_sq.min(axis=0)
+    current_pot = closest_dist_sq.sum()
 
     # Pick the remaining n_clusters points
     for c in range(start_centers, n_clusters):
